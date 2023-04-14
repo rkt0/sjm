4bbf62c258738387dd69dd18893c596fc2f7e647
Ray Thomas <raymond.kyle.thomas.0@gmail.com>
2023-04-13 21:30:04 -0400

    add simple chipmunk behavior

--- gameplay.js
+++ gameplay.js
@@ -1,55 +1,85 @@
 import {qs, qsa, ael, aelo} from './utility.js';
 
-const [fSize, cSize] = ['field', 'chipmunk'].map(
-  x => parseInt(
-    getComputedStyle(qs(':root')).getPropertyValue(
-      `--${x}-size`
+const config = {
+  nChipmunks: 6,
+  chipmunkRate: 1,
+};
+
+// Get field and chipmunk sizes from CSS
+{
+  const [f, c] = ['field', 'chipmunk'].map(
+    x => parseInt(
+      getComputedStyle(qs(':root')).getPropertyValue(
+        `--${x}-size`
+      )
     )
-  )
+  );
+  config.fieldSize = f;
+  config.chipmunkSize = c;
+}
+
+// Distance at which chipmunks are considered "gone"
+config.boundary = Math.SQRT1_2 * (
+  config.fieldSize + config.chipmunkSize
 );
-const ratio = 1 - cSize / fSize;
+
+// Generate random unit vector
+function randUnitVector() {
+  const angle = Math.random() * 2 * Math.PI;
+  return [Math.cos(angle), Math.sin(angle)];
+}
+
+const chipmunks = [];
+function placeChipmunk(chipmunk) {
+  const loc = chipmunk.position.map(
+    u => u * config.boundary
+  );
+  const xf = `translate(${loc[0]}px, ${loc[1]}px)`;
+  chipmunk.element.style.transform = xf;
+}
+function initializeChipmunks() {
+  for (let i = 0; i < config.nChipmunks; i++) {
+    const element = document.createElement('div');
+    element.classList.add('chipmunk');
+    qs('div.gameplay').append(element);
+    const chipmunk = {element};
+    chipmunk.position = [1, 0];
+    chipmunk.velocity = [0, 0];
+    chipmunk.active = false;
+    placeChipmunk(chipmunk);
+    chipmunks.push(chipmunk);
+  }
+}
+function activateChipmunk() {
+  const i = chipmunks.findIndex(c => ! c.active);
+  if (i === -1) {
+    console.log('out of chipmunks'); return;
+  }
+  const chipmunk = chipmunks[i];
+  chipmunk.active = true;
+  chipmunk.position = randUnitVector();
+  chipmunk.velocity = chipmunk.position.map(u => -u);
+  placeChipmunk(chipmunk);
+}
 
 const fpsMeter = {count: 0, time: 0};
 fpsMeter.element = qs('.fps-counter');
-
-const chipmunks = [];
-for (let i = 0; i < 36; i++) {
-  const element = document.createElement('div');
-  element.id = `chipmunk-${i}`;
-  element.classList.add('chipmunk');
-  qs('div.gameplay').append(element);
-  const position = [];
-  for (let j = 0; j < 2; j++) {
-    position.push((Math.random() * 2 - 1) * ratio);
-  }
-  const vAngle = Math.random() * Math.PI * 2;
-  const vSpeed = Math.random() + 1;
-  const velocity = [
-    vSpeed * Math.cos(vAngle), 
-    vSpeed * Math.sin(vAngle),
-  ];
-  chipmunks.push({element, position, velocity});
-}
 
 let oldTimeStamp, stopped;
 function update(timeStamp) {
   if (! oldTimeStamp) oldTimeStamp = timeStamp;
   const elapsed = (timeStamp - oldTimeStamp) / 1000;
+  if (Math.random() < elapsed * config.chipmunkRate) {
+    activateChipmunk();
+  }
   for (const chipmunk of chipmunks) {
+    if (! chipmunk.active) continue;
     const {position, velocity} = chipmunk;
-    const loc = [];
     for (let d = 0; d < 2; d++) {
-      position[d] += velocity[d] * elapsed
-      const overage = Math.abs(position[d]) - ratio;
-      if (overage > 0) {
-        velocity[d] *= -1;
-        position[d] -=
-            overage * 2 * Math.sign(position[d]);
-      }
-      loc[d] = position[d] / 2 * fSize;
+      position[d] += velocity[d] * elapsed;
     }
-    const xf = `translate(${loc[0]}px, ${loc[1]}px)`;
-    chipmunk.element.style.transform = xf;
+    placeChipmunk(chipmunk);
+    chipmunk.active = Math.hypot(...position) < 1;
   }
   fpsMeter.count++;
   fpsMeter.time += elapsed;
@@ -64,10 +94,11 @@ function update(timeStamp) {
   if (! stopped) requestAnimationFrame(update);
 }
 
-requestAnimationFrame(update);
-
 ael('body', 'click', () => {
   stopped = ! stopped;
   oldTimeStamp = null;
   if (! stopped) requestAnimationFrame(update);
 });
+
+initializeChipmunks();
+requestAnimationFrame(update);
