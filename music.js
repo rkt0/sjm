99eea82f34d7c7ecebc40352ca92d677f5390703
rkt0 <raymond.kyle.thomas.0@gmail.com>
2026-03-30 21:48:37 -0400

    refactor music

--- music.js
+++ music.js
@@ -30,103 +30,106 @@ const music = {
     new Track(1, 'Lively Lumpsucker'),
     new Track(1, 'Merry Go'),
   ],
-};
-music.initialize = function () {
-  for (const x of ['mower', 'win', 'ussr']) {
-    this[`${x}Element`] = qs(`audio.${x}`);
-    this[`${x}Element`].src = `audio/${x}.mp3`;
-  }
-  const rString = localStorage.getItem('recentMusic');
-  const rFull = JSON.parse(rString) ?? [];
-  const msPerDay = 1000 * 60 * 60 * 24;
-  const old = Date.now() - msPerDay * this.daysFresh;
-  this.recent = rFull.filter((x) => x.time > old);
-};
-music.start = function () {
-  const last = this.recent.shift();
-  if (!last) this.playTrack(this.firstId);
-  else {
-    const { id, restarts } = last;
-    if (restarts >= this.restartsMax) {
-      this.recent.unshift(last);
-      this.next();
-    } else this.playTrack(id, restarts + 1);
-  }
-};
-music.playTrack = function (id, restarts = 0) {
-  const { element, recent } = this;
-  element.src = this.playlist[id].src;
-  forcePlay(element);
-  recent.unshift({ id, time: Date.now(), restarts });
-  if (recent.length > this.recentMax) recent.pop();
-  const rString = JSON.stringify(recent);
-  localStorage.setItem('recentMusic', rString);
-};
-music.next = function () {
-  const recentIds = this.recent.map((x) => x.id);
-  const weights = this.playlist.map((e, i) =>
-    recentIds.includes(i) ? 0 : e.weight
-  );
-  const n = weights.length;
-  const cdf = [];
-  for (let i = 0; i < n; i++) {
-    cdf.push((cdf[i - 1] ?? 0) + weights[i]);
-  }
-  const total = cdf[n - 1];
-  for (let i = 0; i < n; i++) cdf[i] /= total;
-  const rand = Math.random();
-  let nextId = 0;
-  while (cdf[nextId] < rand) nextId++;
-  this.playTrack(nextId);
-};
-music.fade = function (element = this.element, opts) {
-  const { duration = 3000, steps = 180 } = opts ?? {};
-  const target = +!element.volume;
-  const change = (target - element.volume) / steps;
-  const levels = [];
-  for (let i = 0; i < steps; i++) {
-    // Sounds better if nonlinear
-    levels.push((target - i * change) ** 2);
-  }
-  const timer = setInterval(() => {
-    if (!element.volume) element.play();
-    element.volume = levels.pop();
-    if (!element.volume) element.pause();
-    if (!levels.length) clearInterval(timer);
-  }, duration / steps);
-};
-music.startRevolution = function () {
-  this.ussrElement.src = 'audio/ussr.mp3';
-  this.fade();
-  aelo(this.element, 'pause', () => {
-    this.ussrElement.play();
-  });
-  // In case revolution audio finishes on its own
-  aelo(this.ussrElement, 'ended', () => {
+  storageKey: 'recentMusic',
+  save() {
+    const str = JSON.stringify(this.recent);
+    localStorage.setItem(this.storageKey, str);
+  },
+  load() {
+    const str = localStorage.getItem(this.storageKey);
+    const arr = JSON.parse(str) ?? [];
+    const msPerDay = 1000 * 60 * 60 * 24;
+    const t = Date.now() - msPerDay * this.daysFresh;
+    this.recent = arr.filter((x) => x.time > t);
+  },
+  initialize() {
+    this.load();
+    ael(this.element, 'ended', () => this.next());
+    for (const x of ['mower', 'win', 'ussr']) {
+      this[`${x}Element`] = qs(`audio.${x}`);
+      this[`${x}Element`].src = `audio/${x}.mp3`;
+    }
+  },
+  start() {
+    const last = this.recent.shift();
+    if (!last) this.playTrack(this.firstId);
+    else {
+      const { id, restarts } = last;
+      if (restarts >= this.restartsMax) {
+        this.recent.unshift(last);
+        this.next();
+      } else this.playTrack(id, restarts + 1);
+    }
+  },
+  playTrack(id, restarts = 0) {
+    const { element, recent } = this;
+    element.src = this.playlist[id].src;
+    forcePlay(element);
+    recent.unshift({id, time: Date.now(), restarts});
+    if (recent.length > this.recentMax) recent.pop();
+    this.save();
+  },
+  next() {
+    const recentIds = this.recent.map((x) => x.id);
+    const weights = this.playlist.map(
+      (e, i) => recentIds.includes(i) ? 0 : e.weight
+    );
+    const n = weights.length;
+    const cdf = [];
+    for (let i = 0; i < n; i++) {
+      cdf.push((cdf[i - 1] ?? 0) + weights[i]);
+    }
+    const total = cdf[n - 1];
+    for (let i = 0; i < n; i++) cdf[i] /= total;
+    const rand = Math.random();
+    let nextId = 0;
+    while (cdf[nextId] < rand) nextId++;
+    this.playTrack(nextId);
+  },
+  fade(element = this.element, opts) {
+    const {duration = 3000, steps = 180} = opts ?? {};
+    const target = +!element.volume;
+    const change = (target - element.volume) / steps;
+    const levels = [];
+    for (let i = 0; i < steps; i++) {
+      // Sounds better if nonlinear
+      levels.push((target - i * change) ** 2);
+    }
+    const timer = setInterval(() => {
+      if (!element.volume) element.play();
+      element.volume = levels.pop();
+      if (!element.volume) element.pause();
+      if (!levels.length) clearInterval(timer);
+    }, duration / steps);
+  },
+  startRevolution() {
     this.fade();
-  });
-};
-music.endRevolution = function () {
-  if (this.ussrElement.ended) return;
-  this.fade(this.ussrElement);
-  this.fade();
-};
-music.startMower = function () {
-  this.mowerElement.src ||= 'audio/mower.mp3';
-  this.mowerElement.currentTime = 0;
-  this.mowerElement.play();
-  this.outro = this.mowerElement.duration - 1;
-};
-music.mowing = function () {
-  const current = this.mowerElement.currentTime;
-  const end = this.mowerElement.duration;
-  return 0 < current && current < end;
-};
-music.win = function () {
-  this.winElement.play();
-};
-music.okWin = function () {
-  return this.mowerElement.currentTime > this.outro;
+    const {element, ussrElement} = this;
+    aelo(element, 'pause', () => ussrElement.play());
+    // In case revolution audio finishes on its own
+    aelo(ussrElement, 'ended', () => this.fade());
+  },
+  endRevolution() {
+    if (this.ussrElement.ended) return;
+    this.fade(this.ussrElement);
+    this.fade();
+  },
+  startMower() {
+    this.mowerElement.currentTime = 0;
+    this.mowerElement.play();
+    this.outro = this.mowerElement.duration - 1;
+  },
+  mowing() {
+    const current = this.mowerElement.currentTime;
+    const end = this.mowerElement.duration;
+    return 0 < current && current < end;
+  },
+  win() {
+    this.winElement.play();
+  },
+  okWin() {
+    return this.mowerElement.currentTime > this.outro;
+  },
 };
 
-ael(music.element, 'ended', () => music.next());
+// ael(music.element, 'ended', () => music.next());
